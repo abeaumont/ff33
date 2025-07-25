f679914cd05246467b330cf9ed2c17c2ca12fa82
Sonia Meruelo <smeruelo@gmail.com>
2025-07-26 00:34:43 +0200

    Add mouse events to items

--- scenes/item.gd
+++ scenes/item.gd
@@ -1,3 +1,17 @@
 extends Area2D
 
-# ooenthu
+var defaultCursor = load("res://assets/cursors/Outline/Double/cursor_none.png");
+var itemCursor = load("res://assets/cursors/Outline/Double/cursor_help.png")
+
+signal open_actions_menu(viewport: Node)
+
+func _on_input_event(viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			emit_signal("open_actions_menu", viewport)
+
+func _on_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(itemCursor)
+
+func _on_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(defaultCursor)
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,36 +1,17 @@
 extends Node2D
 
-var defaultCursor = load("res://assets/cursors/Outline/Double/cursor_none.png");
-var itemCursor = load("res://assets/cursors/Outline/Double/cursor_help.png")
+
 const CLICK_RADIUS = 100
 
 var objects = []
 
 func _ready():
-	objects = [$shovel]
-	Input.set_custom_mouse_cursor(defaultCursor)
+	pass
+	#print($Shovel.is_pickable())
+	#print($Shovel.is_visible())
+#	objects = [$shovel]
+#	
 
-func _process(_delta: float) -> void:
-	var pos = get_viewport().get_mouse_position()
-	var found = false
-	for object in objects:
-		if (pos - object.position).length() < CLICK_RADIUS:
-			Input.set_custom_mouse_cursor(itemCursor)
-			found = true
-			break
-	if not found:
-		Input.set_custom_mouse_cursor(defaultCursor)
-		
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_ESCAPE:
-			get_tree().quit()
-		elif event.keycode == KEY_I:
-			$inventory.visible = !$inventory.visible
-	elif event is InputEventMouseButton:
-		var pos = get_viewport().get_mouse_position()
-		for i in range(objects.size()):
-			if (pos - objects[i].position).length() < CLICK_RADIUS:
-				objects[i].hide()
-				objects.remove_at(i)
-				break
+func _on_open_actions_menu(item: Node) -> void:
+	$ActionsMenu.show()
+	print(item)
