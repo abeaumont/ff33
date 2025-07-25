8775f7ca26f143fdc3e96cc129cd31d3635a5dce
Sonia Meruelo <smeruelo@gmail.com>
2025-07-26 01:48:02 +0200

    Change action names

--- scenes/item.gd
+++ scenes/item.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
-var defaultCursor = load("res://assets/cursors/Outline/Double/cursor_none.png");
-var itemCursor = load("res://assets/cursors/Outline/Double/cursor_help.png")
+var defaultCursor = load("res://assets/cursors/Outline/Double/cursor_none.png")
+var itemCursor = load("res://assets/cursors/Outline/Double/target_b.png")
+var description
 
 signal open_actions_menu(item)
 
+	
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-
 const CLICK_RADIUS = 100
+var defaultCursor = load("res://assets/cursors/Outline/Double/cursor_none.png");
 
 var objects = []
 
 func _ready():
-	pass
+	Input.set_custom_mouse_cursor(defaultCursor)
+	
+func _init() -> void:
+	Input.set_custom_mouse_cursor(defaultCursor)
 	#print($Shovel.is_pickable())
 	#print($Shovel.is_visible())
 #	objects = [$shovel]
