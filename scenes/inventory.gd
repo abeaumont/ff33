e1b8113cbbcec99b2b9babad5715c2540a9f1fa1
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2025-07-26 06:59:27 +0200

    Fix default

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 signal on_item_button_clicked(resource_name: String)
 
+func _ready():
+	add("genericItem_color_005")
+
 func add(resource_name):
 	$GridContainer.add_object(resource_name)
 	
