3b1eebd895d5964859098ff7c219af587c4acd49
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2025-07-26 06:58:06 +0200

    Remove noise

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -2,15 +2,7 @@ extends Node2D
 
 signal on_item_button_clicked(resource_name: String)
 
-func _ready():
-	add("genericItem_color_005")
-	
-	
-func _process(_delta):
-	pass
-
 func add(resource_name):
-	print("addition requested!")
 	$GridContainer.add_object(resource_name)
 	
 func remove(resource_name):
@@ -18,7 +10,6 @@ func remove(resource_name):
 
 func _on_item_button_clicked(resource_name: Variant) -> void:
 	emit_signal("on_item_button_clicked", resource_name)
-
 
 func _on_close_pressed() -> void:
 	hide()
