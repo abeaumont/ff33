7ed818912c9ea0dcaad77557b1def516280a88e1
NachoGoro <nachogoro.dev@gmail.com>
2025-07-26 02:00:53 +0200

    add empty and item buttons

--- scenes/grid_container.gd
+++ scenes/grid_container.gd
@@ -1,10 +1,19 @@
 extends GridContainer
 
+var empty_button_scene = preload("res://scenes/EmptyButton.tscn")
+var item_button_scene = preload("res://scenes/ItemButton.tscn")
+
 const N = 9
 
 func _ready():
-	for i in range(N):
-		var button = Button.new()
+	var shovel = item_button_scene.instantiate()
+	shovel.icon = load("res://assets/objects/Colored/genericItem_color_005.png")
+	shovel.disabled = true
+	add_child(shovel)
+	
+	for i in range(1,N):
+		var button = empty_button_scene.instantiate()
 		button.icon = load("res://assets/ui/inventory_panel.png")
 		button.disabled = true
 		add_child(button)
+	pass
---
+++ scenes/item_button.gd
@@ -0,0 +1,9 @@
+extends Button
+
+@onready var item = $Item  # Adjust path as needed
+
+func _ready():
+	pass
+	#item.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	#if item.has_node("Sprite2D"):
+	#	item.get_node("Sprite2D").mouse_filter = Control.MOUSE_FILTER_IGNORE
